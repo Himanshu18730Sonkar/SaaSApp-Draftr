2b07e385f31fc04915827ea9d709c2a7ae5eefd4
Himanshu Sonkar <himanshu18730@gmail.com>
2026-01-15 21:59:39 +0530

    fix: add custom theme colors and spacing to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(240, 3%, 6%)',
+        secondary: 'hsl(225, 3%, 28%)',
+      },
+      maxWidth: {
+        '350': '87.5rem',
+        '77.25': '19.3125rem',
+      },
+      gap: {
+        '30': '7.5rem',
+      },
+      spacing: {
+        '30': '7.5rem',
+      },
+    },
   },
   plugins: [],
 }
